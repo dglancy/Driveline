2bff8884430baa0cbb95e2b8bc1a45ca5f9af952
Damien Glancy <damien.glancy@icloud.com>
2026-05-30 12:30:38 +0100

    #3 Updting app to handle UI testing using memory data store

---
+++ AutoRoute/App/AppLogger.swift
@@ -0,0 +1,8 @@
+//
+//  AppLogger.swift
+//  AutoRoute
+//
+//  Created by Damien Glancy on 30/05/2026.
+//
+
+import Foundation
--- AutoRouteUITests/AutoRouteUITests.swift
+++ AutoRouteUITests/AutoRouteUITests.swift
@@ -7,37 +7,21 @@
 
 import XCTest
 
-final class AutoRouteUITests: XCTestCase {
+final class AutoRouteUITests: BaseXCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+  @MainActor
+  func testExample() throws {
+    app.launch()
 
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+    // XCUIAutomation Documentation
+    // https://developer.apple.com/documentation/xcuiautomation
+  }
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+  @MainActor
+  func testLaunchPerformance() throws {
+    measure(metrics: [XCTApplicationLaunchMetric()]) {
+      app.launch()
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    @MainActor
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // XCUIAutomation Documentation
-        // https://developer.apple.com/documentation/xcuiautomation
-    }
-
-    @MainActor
-    func testLaunchPerformance() throws {
-        // This measures how long it takes to launch your application.
-        measure(metrics: [XCTApplicationLaunchMetric()]) {
-            XCUIApplication().launch()
-        }
-    }
+  }
 }
