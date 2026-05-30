453bbe652213aec99a644d584bd7ca78a38d6a96
Damien Glancy <damien.glancy@icloud.com>
2026-05-30 13:47:35 +0100

    #3 Removing trailing_whitespaces (swiftlint)

--- AutoRoute/AppLifecycle/AutoRouteApp.swift
+++ AutoRoute/AppLifecycle/AutoRouteApp.swift
@@ -10,54 +10,54 @@ import SwiftData
 
 @main
 struct AutoRouteApp: App {
-  
+
   // MARK: - Properties
-  
+
   private let modelContainer: ModelContainer
-  
+
   // MARK: - Lifecycle
-  
+
   init() {
     Log.lifecycle.info("App starting")
     let isUITesting = Self.isUITesting()
-    
+
     modelContainer = Self.createModelContainer(isUITesting: isUITesting)
-    
+
     if isUITesting {
       Log.lifecycle.info("Running in UI Testing mode")
     }
     Log.lifecycle.info("App started")
   }
-  
+
   // MARK: - Main View Scene
-  
+
   var body: some Scene {
     WindowGroup {
       ContentView()
     }
     .modelContainer(modelContainer)
   }
-  
+
   // MARK: - Factories
-  
+
   private static func createModelContainer(isUITesting: Bool) -> ModelContainer {
     Log.lifecycle.info("Setting up data model and container")
     let schema = Schema([
       Route.self,
       Position.self
     ])
-    
+
     let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: isUITesting)
-    
+
     do {
       return try ModelContainer(for: schema, configurations: [modelConfiguration])
     } catch {
       fatalError("Could not create ModelContainer: \(error)")
     }
   }
-  
+
   // MARK: - Private functions
-  
+
   private static func isUITesting() -> Bool {
     ProcessInfo.processInfo.arguments.contains("-ui-testing")
   }
